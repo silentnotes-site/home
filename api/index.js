765a5cfcb7a24e2f66412080f6e35460fbd5a742
silentnotes-site <roblox.italia.official@gmail.com>
2025-07-01 19:29:12 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-const app = require('../server.js')
-module.exports = app
+{
+  "$schema": "https://openapi.vercel.sh/vercel.json",
+  "version": 2,
+  "functions": {
+    "api/index.js": {
+      "runtime": "nodejs18.x"
+    }
+  }
+}
